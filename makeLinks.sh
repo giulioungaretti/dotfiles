5ae06c72532e3d672b5b0e577a8a0b017407f879
giulio <giulioungaretti@me.com>
2016-01-14 17:32:03 +0100

    switch to tmux plugs

--- makeLinks.sh
+++ makeLinks.sh
@@ -19,7 +19,13 @@ rm -f ~/.README.md
 echo done
 
 if [ !  "~/.vim/autoload/plug.vim " ]; then
-        echo set up plug
+        echo 'set up plug'
         curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+
+if [! "~/.tmux/plugins/tpm~/.tmux/plugins/tpm" ]; then
+        echo 'set up tmux plugins'
+ 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
