d948fafa8edf0f6b88fb8e7a03e741b49a2f3ee3
Giulio Ungaretti <giulioungaretti@me.com>
2016-06-22 09:53:53 +0200

    bla much fix

---
+++ install.sh
@@ -0,0 +1,26 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update 
+brew upgrade
+brew 
+brew list tmux
+brew info tmux
+brew install git
+brew install keybase
+brew cask install alfred3
+brew cask install alfred
+brew cask install slack
+brew cask install todoit
+brew cask install todoist
+brew cask install todoist
+brew install mosh
+brew tap neovim/neovim
+brew install --HEAD neovim
+brew install htop
+brew install wget
+brew cask install skype
+brew installl python3
+brew intall python3
+brew install python3
+history | grep brew
+history | grep brew > dotfiles/install.sh
+brew install zsh
