d0a887d867a11369756dcf64004a859326899432
Giulio Ungaretti <giulioungaretti@me.com>
2018-08-14 11:03:47 +0200

    🐠 all the things

---
+++ config/fish/config.fish
@@ -0,0 +1,8 @@
+set -x fish_cursor_default block
+set -x fish_cursor_visual block
+set -x fish_cursor_insert line
+set -x fish_cursor_replace_one underscore
+function fish_mode_prompt; end
+fish_vi_key_bindings
+function fish_mode_prompt; end
+set fish_greeting
