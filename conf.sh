982bb38176be752f342add9a11b0f45f26a33cff
Giulio Ungaretti <giulioungaretti@me.com>
2018-08-16 13:00:41 +0200

    polish install command

--- conf.sh
+++ conf.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
-set -euf 
-echo "setting up vim"
+set -euf
+# git config!
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/git_commit_msg.txt ~/.git_commit_msg.txt
 # first rule vim
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 # then rule inputrc based stuff
@@ -11,23 +13,8 @@ mkdir -p "$HOME/.undodir/"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -sf ~/dotfiles/vimPlug ~/.vimPlug
-echo "setting up tmux"
 # then tmux
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf 
-
-echo 'set up tmux plugins'
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# linux
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "setting up x-server"
-    ln -sf ~/dotfiles/xinitrc ~/.xinitrc 
-    ln -sf ~/dotfiles/Xmodmap ~/.Xmodmap
-fi
-# there may be more linking needed depening on hardware
-# for example touchpads 
-echo "zprezto"
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-
-ln -sf ~/dotfiles/zshrc ~/.zshrc
-ln -sf ~/dotfiles/zpreztorc ~/.zpreztorc
+# add fish
+# add other folders 
